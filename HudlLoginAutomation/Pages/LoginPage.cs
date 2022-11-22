3ad9cac254cf861d06d3b6f086a733022fbefdde
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 16:36:47 -0600

    added runsettings template file and small updates to LoginPage

--- HudlLoginAutomation/Pages/LoginPage.cs
+++ HudlLoginAutomation/Pages/LoginPage.cs
@@ -8,19 +8,24 @@ namespace HudlLoginAutomation.Pages
     internal class LoginPage : BasePage
     {
         /* Web Elements */
-        /*
-        IWebElement emailInput = driver.FindElement(By.Id("email"));
-        IWebElement passwordInput = driver.FindElement(By.Id("password"));
-        IWebElement loginButton = driver.FindElement(By.Id("logIn"));
-        IWebElement rememberMeCheckbox = driver.FindElement(By.Id("uniId_2"));
-        IWebElement needHelpLink = driver.FindElement(By.CssSelector("a[data-qa-id='need-help-link']"));
-        IWebElement signUpLink = driver.FindElement(By.CssSelector("a[href='/register/signup']"));
-        IWebElement organizationLogInButton = driver.FindElement(By.CssSelector("button[data-qa-id='log-in-with-organization-btn']"));
-        */
+        IWebElement emailInput => driver.FindElement(By.Id("email"));
+        IWebElement passwordInput => driver.FindElement(By.Id("password"));
+        IWebElement loginButton => driver.FindElement(By.Id("logIn"));
+        IWebElement rememberMeCheckbox => driver.FindElement(By.Id("uniId_2"));
+        IWebElement needHelpLink => driver.FindElement(By.CssSelector("a[data-qa-id='need-help-link']"));
+        IWebElement signUpLink => driver.FindElement(By.CssSelector("a[href='/register/signup']"));
+        IWebElement organizationLogInButton => driver.FindElement(By.CssSelector("button[data-qa-id='log-in-with-organization-btn']"));
 
         public LoginPage(IWebDriver driver) : base(driver)
         {
             this.driver = driver;
+        }
+
+        public void SignIn()
+        {
+            EnterText(emailInput, "someEmail");
+            EnterText(passwordInput, "somePassword");
+            loginButton.Click();
         }
     }
 }
