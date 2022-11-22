59bc91e0ffb62cff3347b0a508a112721029961c
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 15:00:47 -0600

    Basic Project Foundation

---
+++ HudlLoginAutomation/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HudlLoginAutomation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
