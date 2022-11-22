5751dae7d2c0d503ce3b31c25cae3603aa49be85
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 15:24:52 -0600

    Cleaning Up Login Tests

--- HudlLoginAutomation/Tests/LoginTests.cs
+++ HudlLoginAutomation/Tests/LoginTests.cs
@@ -1,10 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace HudlLoginAutomation.Tests
 {
