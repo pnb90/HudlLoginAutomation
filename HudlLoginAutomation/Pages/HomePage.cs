3f69947cf2b1297da8c7cd8266b57e3dd7906a5d
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 19:09:47 -0600

    Created a homepage

---
+++ HudlLoginAutomation/Pages/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace HudlLoginAutomation.Pages
+{
+    internal class HomePage : BasePage
+    {
+        #region Web Elements
+        IWebElement homeLink => driver.FindElement(By.CssSelector("a[data-qa-id='webnav-globalnav-home']"));
+        IWebElement exploreLink => driver.FindElement(By.CssSelector("a[data-qa-id='webnav-globalnav-explore']"));
+        IWebElement uploadLink => driver.FindElement(By.CssSelector("a[data-qa-id='webnav-globalnav-upload']"));
+        IWebElement notificationsLink => driver.FindElement(By.Id("notifications-link"));
+        IWebElement messagesLink => driver.FindElement(By.CssSelector("a[data-qa-id='webnav-globalnav-messages']"));
+
+        #endregion
+
+        #region Constructor
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+        #endregion
+    }
+}
