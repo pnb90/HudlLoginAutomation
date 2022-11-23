248568b1961aee1de3d4c4dac2f8fbec15e666ed
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 18:30:17 -0600

    Getting started on writing loginTests

--- HudlLoginAutomation/Tests/LoginTests.cs
+++ HudlLoginAutomation/Tests/LoginTests.cs
@@ -1,35 +1,54 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using NUnit.Framework;
+using HudlLoginAutomation.Pages;
 
 namespace HudlLoginAutomation.Tests
 {
     internal class LoginTests
     {
+        #region Member Variables
         IWebDriver driver;
+        String email = TestContext.Parameters.Get("email");
+        String password = TestContext.Parameters.Get("password");
+        #endregion
 
+        #region Constructor
         public LoginTests()
         {
         }
+        #endregion
 
+
+        #region Setup
         [SetUp]
         public void SetUp()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-        }
-
-        [Test]
-        public void FirstTest()
-        {
             driver.Navigate().GoToUrl("https://hudl.com/login");
+        }
+        #endregion
 
+
+        #region Tests
+        [Test]
+        public void ValidEmailAndPassword()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.SignIn(email, password);
+            Thread.Sleep(5);
         }
 
+        #endregion
+
+
+        #region Teardown
         [TearDown]
         public void TearDown()
         {
             driver.Quit();
         }
+        #endregion
     }
 }
