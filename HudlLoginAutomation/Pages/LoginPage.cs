9b4aa43f03f9ca6ffdc0ed7f644cfed0b3652807
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 18:29:44 -0600

    Added regions for clarity on LoginPage

--- HudlLoginAutomation/Pages/LoginPage.cs
+++ HudlLoginAutomation/Pages/LoginPage.cs
@@ -4,7 +4,7 @@ namespace HudlLoginAutomation.Pages
 {
     internal class LoginPage : BasePage
     {
-        /* Web Elements */
+        #region Web Elements
         IWebElement emailInput => driver.FindElement(By.Id("email"));
         IWebElement passwordInput => driver.FindElement(By.Id("password"));
         IWebElement loginButton => driver.FindElement(By.Id("logIn"));
@@ -12,17 +12,23 @@ namespace HudlLoginAutomation.Pages
         IWebElement needHelpLink => driver.FindElement(By.CssSelector("a[data-qa-id='need-help-link']"));
         IWebElement signUpLink => driver.FindElement(By.CssSelector("a[href='/register/signup']"));
         IWebElement organizationLogInButton => driver.FindElement(By.CssSelector("button[data-qa-id='log-in-with-organization-btn']"));
+        #endregion
 
+        #region Constructor
         public LoginPage(IWebDriver driver) : base(driver)
         {
             this.driver = driver;
         }
+        #endregion
 
+        #region Public Methods 
         public void SignIn(String email, String password)
         {
             EnterText(emailInput, email);
             EnterText(passwordInput, password);
             loginButton.Click();
         }
+        #endregion
+
     }
 }
