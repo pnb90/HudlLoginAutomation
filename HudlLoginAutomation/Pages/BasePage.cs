f0c1960063b07dcf8854e27db4d62f58c6978e2d
Phuoc Bui <phuocbui90@gmail.com>
2022-11-22 18:29:34 -0600

    Added regions for clarity to BasePage

--- HudlLoginAutomation/Pages/BasePage.cs
+++ HudlLoginAutomation/Pages/BasePage.cs
@@ -10,19 +10,29 @@ namespace HudlLoginAutomation.Pages
 {
     public class BasePage
     {
+        #region Member Variables
         public IWebDriver driver { get; set; }
         public WebDriverWait wait;
+        #endregion
+
+        #region Constructors
 
         public BasePage(IWebDriver driver)
         {
             this.driver = driver;
             this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(5));
         }
+        #endregion
 
-        public void EnterText(WebElement element, String text)
+        #region Public Methods
+        public void EnterText(IWebElement element, String text)
         {
             element.Clear();
             element.SendKeys(text);
         }
+        #endregion
+
+        #region Private Methods
+        #endregion
     }
 }
